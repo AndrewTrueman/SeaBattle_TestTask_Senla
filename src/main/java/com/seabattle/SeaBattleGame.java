3ff793a40f3bce44505dba0f473fc0f0410d7ae0
candy <andrewmail2001@gmail.com>
2024-02-04 20:48:27 +0300

    Added option exit the game in main menu

--- src/main/java/com/seabattle/SeaBattleGame.java
+++ src/main/java/com/seabattle/SeaBattleGame.java
@@ -6,6 +6,8 @@ public class SeaBattleGame {
         System.out.println("Выберите режим игры:");
         System.out.println("1. Игра с другом.");
         System.out.println("2. Игра с компьютером.");
+        System.out.println("3. Завершить игру.");
+        System.out.println("Вы всегда можете завершить игру написав команду Break");// пока не реализовано
         Scanner scanner = new Scanner(System.in);
         int gameMode = scanner.nextInt();
 
@@ -15,8 +17,9 @@ public class SeaBattleGame {
         } else if (gameMode == 2) {
             ComputerPlayerGame computerPlayerGame = new ComputerPlayerGame();
             computerPlayerGame.play();
-        } else {
-            System.out.println("Неверный режим. Завершение программы.");
-        }
+        } else if (gameMode == 3){
+            System.out.println("Игра завершена. До свидания!");
+            System.exit(0);  // Завершение программы
+        }else  System.out.println("Неверная опция. Пожалуйста, выберите существующую опцию.");
     }
 }
