0bdadad96ccea7476a5d37f06a414d383d1c29f4
Andrew <49242913+AndrewTrueman@users.noreply.github.com>
2024-02-04 20:35:25 +0300

    Update start.bat
    Run in through classpath to avorid manifest error

--- out/artifacts/SeaBattle_TestTask_jar/start.bat
+++ out/artifacts/SeaBattle_TestTask_jar/start.bat
@@ -1,3 +1,3 @@
 @echo off
-java -classpath "D:\Work\SeaBattle_TestTask\out\artifacts\SeaBattle_TestTask_jar\SeaBattle_TestTask.jar" com.seabattle.SeaBattleGame
+java -classpath SeaBattle_TestTask.jar com.seabattle.SeaBattleGame
 pause
